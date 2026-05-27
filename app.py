07e3596a5ae951f0cc43a2035d2b4057376636da
Arun George <arungeorge1703@gmail.com>
2026-05-28 02:31:24 +0530

    Chatbot with memory

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
 from ollama import chat
 
-user_input = input("You: ")
+messages = []
 
-response = chat(
-    model="llama3.2:1b",
-    messages=[
+while True:
+
+    # Get user input
+    user_input = input("You: ")
+
+    # Exit condition
+    if (user_input.lower() == "exit") or (user_input.lower() == "end"):
+        print("Goodbye!")
+        break
+
+    # Store user message
+    messages.append(
         {
             "role": "user",
             "content": user_input
         }
-    ]
-)
+    )
 
-print("AI:", response["message"]["content"])
+    # Send full conversation to model
+    response = chat(
+        model="llama3.2:1b",
+        messages=messages
+    )
+
+    # Extract assistant reply
+    assistant_message = response["message"]
+
+    # Print assistant response
+    print("AI:", assistant_message["content"])
+
+    # Store assistant reply in memory
+    messages.append(assistant_message)
